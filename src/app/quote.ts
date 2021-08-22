3cf486c488ed6f65b4050ab8bb7920939500cdb1
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-08-22 15:26:45 +0300

    Add welcome page

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,6 @@
 export class Quote {
-    constructor (public id :number, public name :string,public author :string,public fan :string ,public upvotes :number ,public downvotes :number ,public notedDate:Date  )
+    showDetails:boolean
+    constructor(public id :number, public name :string,public author :string,public fan :string ,public upvotes :number ,public downvotes :number , public notedDate:Date  ){
+        this.showDetails=false
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] =[
+    new Quote (1,"Happiness quite unshared can scarcely be called happiness'then it has no taste.","Charlotte Bronte","Amira",0,0,new Date(2018,10,17))
+  ]
 
   constructor() { }
 
