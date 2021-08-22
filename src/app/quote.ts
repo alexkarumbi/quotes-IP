4e57627a010f2f922cf4f66e3f553fc78652c988
Alex Karumbi <alex.muhia@student.moringaschool.com>
2021-08-22 14:42:42 +0300

    Add base files

---
+++ src/app/quote.ts
@@ -0,0 +1,3 @@
+export class Quote {
+    constructor (public id :number, public name :string,public author :string,public fan :string ,public upvotes :number ,public downvotes :number ,public notedDate:Date  )
+}
